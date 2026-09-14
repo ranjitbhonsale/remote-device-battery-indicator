b40631a1a5a76552bbd0cf9aae36526d6524205e
Ranjit Bhonsale <ranjit.bhonsale@gmail.com>
2026-09-14 23:26:59 +0530

    Deploy version 2.7 (versionCode 18) to Google Play Store

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "work.ranjit.batteryntfy"
         minSdk = 23
         targetSdk = 36
-        versionCode = 17
-        versionName = "2.6"
+        versionCode = 18
+        versionName = "2.7"
     }
 
     signingConfigs {
